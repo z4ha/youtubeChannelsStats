1d5a16d5b4bf2982975dffc70799aa0a414b8cbe
z4ha <jakmusze@gmail.com>
2017-09-17 01:46:21 +0200

    Add request and JSON selector

---
+++ app.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+var express = require('express');
+var request = require('request');
+var channelName = 'reZiPlayGamesAgain';
+api = process.env.API;
+database = process.env.DATABASE;
+
+app = express();
+
+function queryMaker(channel, api){
+  return 'https://www.googleapis.com/youtube/v3/channels?part=statistics&forUsername='+channel+'&key=' + api;
+}
+
+request(queryMaker(channelName, api), function (error, response, body) {
+  if(!error && response.statusCode == 200){
+    var parsedBody = JSON.parse(body);
+    console.log(parsedBody.items[0].statistics)
+  }
+});
+
+
+
+var port = process.env.PORT || 3000;
+app.listen(port);
+console.log('Running ' + port);
